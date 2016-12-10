29331499b2ac62377cc084ab48bbd1660a7da14a
Aljaz <aljaz.blazej@gmail.com>
2016-12-10 17:20:23 +0100

    Zombie movement added

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance = null;
+
+	public Transform CharacterTransform = null;
+
+	void Awake() {
+		Instance = this;
+	}
+}
---
+++ Assets/scripts/ZombieMovementController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMovementController : MonoBehaviour {
+
+	[SerializeField] private float MoveSpeed;
+	private Transform characterTransform = null;
+
+	void Start() {
+		characterTransform = GameController.Instance.CharacterTransform;
+	}
+
+	void Update () {
+		transform.position += Vector3.Normalize(characterTransform.position - transform.position) * Time.deltaTime; 
+		transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+	}
+}
