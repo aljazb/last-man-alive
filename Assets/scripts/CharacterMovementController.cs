296f3a77cbd22e581fabd831656b1cec13a5acdf
Aljaz <aljaz.blazej@gmail.com>
2016-12-10 16:09:50 +0100

    Added walls, character placeholder, temp movement

---
+++ Assets/scripts/CharacterMovementController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementController : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+
+	void Update () {
+		float vertMovement = Input.GetAxis("Vertical");
+		float horiMovement = Input.GetAxis("Horizontal");
+		transform.position += new Vector3(horiMovement * moveSpeed * Time.deltaTime,0,vertMovement * moveSpeed * Time.deltaTime);
+	}
+}
