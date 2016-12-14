db3bb945b5244f10d8aceb954bd7d7ce254254b2
Aljaz <aljaz.blazej@gmail.com>
2016-12-14 14:14:13 +0100

    Graves added, zombie logic improvements

---
+++ Assets/scripts/ZombieLifeController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieLifeController : MonoBehaviour {
+
+	[SerializeField] private Rigidbody Rigidbody;
+	[SerializeField] private float SinkSpeed;
+	[SerializeField] private float SinkDelay;
+	[SerializeField] private GameObject DirtParticleSystem;
+	[SerializeField] private Transform ZombieCenter;
+	[SerializeField] private Animator Animator;
+	[SerializeField] private int LifeCount;
+
+	private bool SinkToGround = false;
+	public bool Dead = false;
+
+	void Update() {
+		if (SinkToGround) {
+			transform.position += Vector3.down * SinkSpeed * Time.deltaTime;
+			if (transform.position.y < -3f) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Bullet") {
+			LifeCount--;
+			if (LifeCount <= 0) {
+				Animator.SetTrigger("Stop");
+				Dead = true;
+				Rigidbody.useGravity = true;
+				Rigidbody.constraints = RigidbodyConstraints.None;
+				StartCoroutine(DelaySink());
+			}
+		}
+	}
+
+	IEnumerator DelaySink() {
+		yield return new WaitForSeconds(SinkDelay);
+		Instantiate(DirtParticleSystem, ZombieCenter.position, DirtParticleSystem.transform.rotation);
+		SinkToGround = true;
+		Rigidbody.useGravity = false;
+		GetComponent<BoxCollider>().enabled = false;
+		GetComponent<CapsuleCollider>().enabled = false;
+	}
+}
--- Assets/scripts/ZombieMovementController.cs
+++ Assets/scripts/ZombieMovementController.cs
@@ -6,21 +6,14 @@ public class ZombieMovementController : MonoBehaviour {
 	[SerializeField] private float MoveSpeed;
 	private Transform characterTransform = null;
 	[SerializeField] private Rigidbody Rigidbody;
-	[SerializeField] private float SinkSpeed;
-	[SerializeField] private float SinkDelay;
-	[SerializeField] private GameObject DirtParticleSystem;
-	[SerializeField] private Transform ZombieCenter;
-	[SerializeField] private Animator Animator;
-
-	private bool Dead = false;
-	private bool SinkToGround = false;
+	[SerializeField] private ZombieLifeController ZombieLifeController;
 
 	void Start() {
 		characterTransform = GameController.Instance.CharacterTransform;
 	}
 
 	void FixedUpdate () {
-		if (!Dead) {
+		if (!ZombieLifeController.Dead) {
 			Vector3 movement = Vector3.Normalize(characterTransform.position - transform.position) * MoveSpeed;
 			Rigidbody.velocity = Vector3.zero;
 			Rigidbody.AddForce(movement.x, 0, movement.z); 
@@ -28,13 +21,6 @@ public class ZombieMovementController : MonoBehaviour {
 	}
 
 	void Update() {
-		if (SinkToGround) {
-			transform.position += Vector3.down * SinkSpeed * Time.deltaTime;
-			if (transform.position.y < -3f) {
-				Destroy(gameObject);
-			}
-		}
-
 		float charX = characterTransform.position.x;
 		float charY = characterTransform.position.z;
 		float zombieX = transform.position.x;
@@ -44,27 +30,8 @@ public class ZombieMovementController : MonoBehaviour {
 
 		float angle = Mathf.Atan2(norm_vector.x, norm_vector.y) * Mathf.Rad2Deg;
 
-		if (!Dead) {
+		if (!ZombieLifeController.Dead) {
 			transform.eulerAngles = new Vector3 (0, angle, 0);
 		}
-	}
- 
-	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.tag == "Bullet") {
-			Animator.SetTrigger("Stop");
-			Dead = true;
-			Rigidbody.useGravity = true;
-			Rigidbody.constraints = RigidbodyConstraints.None;
-			StartCoroutine(DelaySink());
-		}
-	}
-
-	IEnumerator DelaySink() {
-		yield return new WaitForSeconds(SinkDelay);
-		Instantiate(DirtParticleSystem, ZombieCenter.position, DirtParticleSystem.transform.rotation);
-		SinkToGround = true;
-		Rigidbody.useGravity = false;
-		GetComponent<BoxCollider>().enabled = false;
-		GetComponent<CapsuleCollider>().enabled = false;
 	}
 }
