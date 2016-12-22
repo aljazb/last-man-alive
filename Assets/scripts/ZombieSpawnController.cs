7c44230f2c620e7a9a22ea6c555ae81740edcc06
Unknown <mz4730@student.uni-lj.si>
2016-12-22 15:50:58 +0100

    Panels for zombies and bullets working

--- Assets/scripts/ZombieSpawnController.cs
+++ Assets/scripts/ZombieSpawnController.cs
@@ -2,21 +2,47 @@
 using System.Collections;
 
 public class ZombieSpawnController : MonoBehaviour {
+	[SerializeField] private int MaxZombies;
+	[SerializeField] private float NewZombieTime;
+	[SerializeField] private int NumOfZombies;
+	[SerializeField] private UnityEngine.UI.Text NumOfZombiesText;
+	private float StartTime;
 
 	[SerializeField] private Camera camera;
 	[SerializeField] private GameObject zombiePrefab;
 
 	void Start () {
-	
+		StartTime = Time.time;
 	}
 		
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetButtonDown("Fire1")  && NumOfZombies > 0) {
 			RaycastHit hit;
 			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.name == "ZombieArea") {
 				Instantiate(zombiePrefab, hit.point, zombiePrefab.transform.rotation);
+				DeleteZombie ();
 			}
+		}
+
+		float CurrentTime = Time.time;
+		if (CurrentTime - StartTime > NewZombieTime) {
+			StartTime = CurrentTime;
+			AddZombie();
+		}
+
+		NumOfZombiesText.text = NumOfZombies.ToString();
+	}
+
+	void AddZombie() {
+		if (NumOfZombies < MaxZombies) {
+			NumOfZombies++;
+		}
+	}
+
+	void DeleteZombie() {
+		if (NumOfZombies > 0) {
+			NumOfZombies--;
 		}
 	}
 }
