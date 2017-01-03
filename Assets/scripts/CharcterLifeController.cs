6778bdc090488ece99a51813fe44920af68dd1eb
Aljaz <aljaz.blazej@gmail.com>
2017-01-03 16:40:55 +0100

    Added life to player

---
+++ Assets/scripts/CharcterLifeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharcterLifeController : MonoBehaviour {
+	[SerializeField] private float CharacterLife;
+	[SerializeField] private float ZombieDamage;
+	[SerializeField] private float BigDaddyDamage;
+	[SerializeField] private Slider Slider;
+
+	void Start () {
+		CharacterLife = 1;
+	}
+
+	void lifeDamaged(bool isBigDaddy) {
+		if (IsAlive()) {
+			if (isBigDaddy) {
+				CharacterLife -= BigDaddyDamage * Time.fixedDeltaTime;
+			}
+			else {
+				CharacterLife -= ZombieDamage * Time.fixedDeltaTime;
+			}
+			Slider.value = CharacterLife;
+		}
+		else {
+			Die();
+		}
+	} 
+
+	bool IsAlive() {
+		return CharacterLife > 0;
+	}
+
+	void Die() {
+		
+	}
+
+	void OnCollisionStay(Collision col) {
+		if (col.gameObject.tag == "Zombie") {
+			lifeDamaged(false);
+		} else if (col.gameObject.tag == "BigDaddy") {
+			lifeDamaged(true);
+		}
+	}
+}
