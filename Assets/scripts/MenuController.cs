a9e543b94607cd9962a96946444343fab92667b8
Aljaz <aljaz.blazej@gmail.com>
2017-01-07 19:13:18 +0100

    Menu added, gui improvements

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadGame() {
+		SceneManager.LoadScene("MainScene");
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
---
+++ Assets/scripts/MenuZombieSpawnController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuZombieSpawnController : MonoBehaviour {
+
+	[SerializeField] private GameObject ZombiePrefab;
+
+	void Start () {
+		for (int i = 0; i < 300; i++) {
+			Instantiate(ZombiePrefab, transform.position + new Vector3(Random.Range(-200f, 200f), 0, Random.Range(-200f, 200f)), Quaternion.identity);
+		}
+	}
+}
